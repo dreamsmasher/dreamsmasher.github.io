a600795e3526abaa298219d16d962dc48dcbe0fd
Norman Liu <normanwuweiliu@gmail.com>
2020-10-24 22:40:16 -0700

    todo: fix CSS

--- site.hs
+++ site.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Hakyll.Core.Configuration
 
 
 --------------------------------------------------------------------------------
